9a682ecfc6ce46c3db7b37574c4247fca08ceeae
mohamed osama <monkosama1996@gmail.com>
2018-02-25 22:02:59 +0200

    splash

--- app/src/main/java/com/example/recyclator/recyclator/SplashActivity.java
+++ app/src/main/java/com/example/recyclator/recyclator/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.recyclator.recyclator;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final Intent intent = new Intent(this, MainActivity.class);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+
+                    Thread.sleep(3000);
+                    startActivity(intent);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finish();
+            }
+        }).start();
     }
-}
+
+
+    }
+
