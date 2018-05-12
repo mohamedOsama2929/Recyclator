119dfb013e7cb828ff393829f8e92b68bce101ef
dewidar <doyderm2299@gmail.com>
2018-05-12 23:10:55 +0200

    dewa

--- app/src/main/java/com/example/recyclator/recyclator/SignUp/SignUpActivity.java
+++ app/src/main/java/com/example/recyclator/recyclator/SignUp/SignUpActivity.java
@@ -9,10 +9,18 @@ import android.widget.Toast;
 
 import com.example.recyclator.recyclator.R;
 import com.example.recyclator.recyclator.placepicActvity;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+//implement it's own interface to use it's method
 public class SignUpActivity extends AppCompatActivity implements ISignUpContract.IsignUpView {
 
     private EditText txtMailEdit, txtPasswordEdit, txtCompanyNameEdit, txtLocationEdit, txtPhoneNumberEdit;
@@ -27,6 +35,7 @@ public class SignUpActivity extends AppCompatActivity implements ISignUpContract
         ButterKnife.bind(this);
 
         mSignUpPresenter = new SignUpPresenter(this);
+
         txtCompanyNameEdit = (EditText) findViewById(R.id.txtMailEdit);
         txtPasswordEdit = (EditText) findViewById(R.id.txtPasswordEdit);
         txtMailEdit = (EditText) findViewById(R.id.txtSignin);
@@ -38,11 +47,16 @@ public class SignUpActivity extends AppCompatActivity implements ISignUpContract
 
     @OnClick(R.id.btnSignin)
     void gotoMap() {
-        // mSignUpPresenter.validateCred(txtCompanyNameEdit.getText().toString().trim(),
-        //       txtPasswordEdit.getText().toString().trim(),txtMailEdit.getText().toString().trim(),
-        //     txtPhoneNumberEdit.getText().toString().trim(),txtLocationEdit.getText().toString().trim());
+        mSignUpPresenter.validateCred(txtCompanyNameEdit.getText().toString().trim(),
+                txtPasswordEdit.getText().toString().trim(), txtMailEdit.getText().toString().trim(),
+                txtPhoneNumberEdit.getText().toString().trim(), txtLocationEdit.getText().toString().trim());
+
+    }
+    @OnClick(R.id.txtSignup)
+    void  gotofrag(){
         Intent i = new Intent(getApplicationContext(), placepicActvity.class);
         startActivity(i);
+
     }
 
     @Override
